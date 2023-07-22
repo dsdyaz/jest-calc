ec060aef079db3f01ce4402914ef37e386b5fe02
dsdy <dsdy06@gmail.com>
2023-07-22 08:34:58 +0300

    add buttons to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Buttons from './components/buttons/buttons'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="app">
+      <Buttons />
+    </div>
   )
 }
 
