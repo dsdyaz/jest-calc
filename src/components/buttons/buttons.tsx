dca767cff843c87d30c315d34c69af1630e22ff7
dsdyaz <dsdy06@gmail.com>
2023-07-23 15:53:37 +0300

    add buttons grid to buttons

--- src/components/buttons/buttons.tsx
+++ src/components/buttons/buttons.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 import "../digit-buttons/digit-arr"
+import "./buttons.css"
 import DigitArr from "../digit-buttons/digit-arr"
 
 export default function Buttons(): React.JSX.Element {
     return (
         <div data-testid="buttons-component" className="buttons">
             {DigitArr()}
+            <button>,</button>
         </div>
     )
 }
