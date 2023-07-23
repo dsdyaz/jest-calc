a5c2c3c9fb68c54bb0cbe73159be2b3c7da049e8
dsdyaz <dsdy06@gmail.com>
2023-07-23 16:05:39 +0300

    fix tests to be more accurate

--- src/components/digit-buttons/tests/digit-arr.test.tsx
+++ src/components/digit-buttons/tests/digit-arr.test.tsx
@@ -5,12 +5,12 @@ test('length of an array is 10', () => {
     expect(DigitArr()).toHaveLength(10)
 })
 
-const expectedButton = <button key={1}>{1}</button>
+const expectedButton = <button className="digit-button digit-button_1" key={1}>{1}</button>
 test('first item in this array is a button with key equal to 1, and 1 passed as content', () => {
     expect(JSON.stringify(DigitArr()[0])).toStrictEqual(JSON.stringify(expectedButton))
 })
 
-const secondExpectedButton = <button key={0}>{0}</button>
+const secondExpectedButton = <button className="digit-button digit-button_0" key={0}>{0}</button>
 test('tenth item in this array is a button with key equal to 0, and 0 passed as content', () => {
     expect(JSON.stringify(DigitArr()[9])).toStrictEqual(JSON.stringify(secondExpectedButton))
 })
