f43363f3056f4ca24ed535574fb9742318d1174a
dsdyaz <dsdy06@gmail.com>
2023-07-23 15:52:43 +0300

    add classnames for digits buttons

--- src/components/digit-buttons/digit-arr.tsx
+++ src/components/digit-buttons/digit-arr.tsx
@@ -4,9 +4,9 @@ export default function DigitArr(): React.JSX.Element[] {
     const digits: React.JSX.Element[] = []
 
     for(let i = 1; i < 10; i++) {
-        digits.push(<button key={i}>{i}</button>)
+        digits.push(<button className={`digit-button digit-button_${i}`} key={i}>{i}</button>)
     }
-    digits.push(<button key={0}>{0}</button>)
+    digits.push(<button className="digit-button digit-button_0" key={0}>{0}</button>)
 
     return digits
 }
