2fa8af9eaf45b43a8a292a92b4e6ac6897e433d0
dsdyaz <dsdy06@gmail.com>
2023-07-24 09:13:02 +0300

    make control buttons

---
+++ src/components/control-buttons/control-buttons.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Controls(): React.JSX.Element {
+    return(
+        <div className="controls" data-testid="control-buttons">
+            <button className="control-button">AC</button>
+            <button className="control-button">±</button>
+            <button className="control-button">%</button>
+        </div>
+    )
+}
