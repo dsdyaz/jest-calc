f5e50bbc0e78acfc9505d58f5ccc795e91359aa3
dsdyaz <dsdy06@gmail.com>
2023-07-28 10:15:50 +0300

    add tests for operators buttons

---
+++ src/components/operators/tests/operators.test.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { test, expect } from "@jest/globals"
+import { render, screen } from "@testing-library/react"
+import Operators from "../operators.tsx"
+
+test("component is in the document", () => {
+  render(<Operators />)
+  expect(screen.getByTestId("operators-buttons")).toBeInTheDocument()
+})
+
+test("component is visible", () => {
+  render(<Operators />)
+  expect(screen.getByTestId("operators-buttons")).toBeVisible()
+})
+
+test("component is not empty", () => {
+  render(<Operators />)
+  expect(screen.getByTestId("operators-buttons")).not.toBeEmptyDOMElement()
+})
