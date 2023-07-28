fbace426bda7d74e2a73d75826b8dad4ab9a23bd
dsdyaz <dsdy06@gmail.com>
2023-07-28 10:38:02 +0300

    add operators and delete button to the component

--- src/components/buttons/buttons.tsx
+++ src/components/buttons/buttons.tsx
@@ -1,13 +1,21 @@
 import React from "react"
 import "./buttons.css"
+import Operators from "../operators/operators.tsx"
 import DigitArr from "../digit-buttons/digit-arr.tsx"
 
 export default function Buttons(): React.JSX.Element {
   return (
     <div data-testid="buttons-component" className="buttons">
-      <div className="buttons__digits">
+      <div className="buttons__main">
         {DigitArr()}
         <button type="button">,</button>
+        <button type="button" className="button-delete">
+          DEL
+        </button>
+      </div>
+
+      <div className="buttons__operators">
+        <Operators />
       </div>
     </div>
   )
