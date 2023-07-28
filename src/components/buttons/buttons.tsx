e3bb0617e8c5d04c5a75c50c7c6398a1514d3477
dsdyaz <dsdy06@gmail.com>
2023-07-28 10:10:54 +0300

    code style fix

--- src/components/buttons/buttons.tsx
+++ src/components/buttons/buttons.tsx
@@ -1,16 +1,14 @@
-import React from "react";
-import "../digit-buttons/digit-arr";
-import "./buttons.css";
-import DigitArr from "../digit-buttons/digit-arr";
-import Controls from "../control-buttons/control-buttons";
+import React from "react"
+import "./buttons.css"
+import DigitArr from "../digit-buttons/digit-arr.tsx"
 
 export default function Buttons(): React.JSX.Element {
   return (
     <div data-testid="buttons-component" className="buttons">
       <div className="buttons__digits">
         {DigitArr()}
-        <button>,</button>
+        <button type="button">,</button>
       </div>
     </div>
-  );
+  )
 }
--- src/components/buttons/tests/buttons.test.tsx
+++ src/components/buttons/tests/buttons.test.tsx
@@ -1,15 +1,16 @@
-import React from "react";
-import Buttons from "../buttons";
-import { render, screen } from "@testing-library/react";
+import React from "react"
+import { test, expect } from "@jest/globals"
+import { render, screen } from "@testing-library/react"
+import Buttons from "../buttons.tsx"
 
 test("component is in the document", () => {
-  render(<Buttons />);
-  expect(screen.getByTestId("buttons-component")).toBeInTheDocument();
-});
+  render(<Buttons />)
+  expect(screen.getByTestId("buttons-component")).toBeInTheDocument()
+})
 
 test("component has button in it", () => {
-  render(<Buttons />);
+  render(<Buttons />)
   expect(screen.getByTestId("buttons-component")).toContainHTML(
-    "<button class='digit-button digit-button_0'>0</button>",
-  );
-});
+    "<button type='button' class='digit-button digit-button_0'>0</button>",
+  )
+})
