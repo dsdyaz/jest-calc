a10e336479cb21d7bce5852117d102de26eeb6b2
dsdyaz <dsdy06@gmail.com>
2023-07-28 10:07:18 +0300

    style fixes

--- src/components/digit-buttons/digit-arr.tsx
+++ src/components/digit-buttons/digit-arr.tsx
@@ -1,20 +1,24 @@
-import React from "react";
+import React from "react"
 
 export default function DigitArr(): React.JSX.Element[] {
-  const digits: React.JSX.Element[] = [];
+  const digits: React.JSX.Element[] = []
 
-  for (let i = 1; i < 10; i++) {
+  for (let i = 1; i < 10; i += 1) {
     digits.push(
-      <button className={`digit-button digit-button_${i}`} key={i}>
+      <button
+        type="button"
+        className={`digit-button digit-button_${i}`}
+        key={i}
+      >
         {i}
       </button>,
-    );
+    )
   }
   digits.push(
-    <button className="digit-button digit-button_0" key={0}>
+    <button type="button" className="digit-button digit-button_0" key={0}>
       {0}
     </button>,
-  );
+  )
 
-  return digits;
+  return digits
 }
