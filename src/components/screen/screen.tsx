59466abaf37b20d5360701caae7ea8d18df87b70
dsdyaz <dsdy06@gmail.com>
2023-07-30 13:32:51 +0300

    add default props to screen component

--- src/components/screen/screen.tsx
+++ src/components/screen/screen.tsx
@@ -1,15 +1,22 @@
 import React from "react"
 
 type ScreenProps = {
-  preResult: string
-  calculation: string
+  preResult?: string
+  calculation?: string
+}
+
+const defaultProps = {
+  preResult: "(0)",
+  calculation: "0",
 }
 
 export default function Screen(props: ScreenProps): React.JSX.Element {
   const { preResult, calculation } = props
   return (
-    <div className="screen">
+    <div data-testid="screen" className="screen">
       <span>{preResult}</span> {calculation}
     </div>
   )
 }
+
+Screen.defaultProps = defaultProps
