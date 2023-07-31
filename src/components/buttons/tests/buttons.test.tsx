99ecc5f970fbbd45d05507939b5c261a09785043
dsdyaz <dsdy06@gmail.com>
2023-07-31 18:05:19 +0300

    ignore tsc throwing error on warnings

--- src/components/buttons/tests/buttons.test.tsx
+++ src/components/buttons/tests/buttons.test.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import React from "react"
 import { test, expect } from "@jest/globals"
 import { render, screen } from "@testing-library/react"
