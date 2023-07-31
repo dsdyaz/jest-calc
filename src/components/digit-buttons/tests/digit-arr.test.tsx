db55cb6f7cb72941cd6b49c78e7d74421b4758ef
dsdyaz <dsdy06@gmail.com>
2023-07-31 18:06:32 +0300

    ignore "unused" react error

--- src/components/digit-buttons/tests/digit-arr.test.tsx
+++ src/components/digit-buttons/tests/digit-arr.test.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import React from "react"
 import { test, expect } from "@jest/globals"
 import DigitArr from "../digit-arr.tsx"
